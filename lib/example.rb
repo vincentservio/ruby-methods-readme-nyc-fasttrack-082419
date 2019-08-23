f1afbda75ac6ef077dd5194e91241077c1b65b0c
vincent servio <vincentservio@gmail.com>
2019-08-23 23:25:17 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,7 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
+def say_hello_world_ten_times
+ phrase = "Hello World!"
 puts phrase
 puts phrase
 puts phrase
@@ -11,3 +12,9 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
+say_hello_world_ten_times
+
+def greeting
+   puts "Hello World"
+end 
